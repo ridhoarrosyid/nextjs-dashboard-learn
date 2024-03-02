3e997749757dcd0ad062f89419c35d56b5d0592f
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2024-03-02 23:51:58 +0700

    finish next js learning path

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <p>customer page</p>;
+import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'customer',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || '';
+  const customer = await fetchFilteredCustomers(query);
+  return <Table customers={customer} />;
 }
